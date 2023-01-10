2bf31ad58afa2d9c019f860257817b99ee67d73d
García Hernández Sergio <axpe.shernandez@externomf.es>
2023-01-10 08:31:30 +0100

    Primer commit. Plugin importado pero sin funcionar.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var cordova: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  param1: string = '';
+  param2: string = '';
 
   constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  testAdd(a: string, b: string) {
+    let data = {
+      param1: a,
+      param2: b
+    }
+
+    cordova.plugins.MathCalculator.add(data).then( (result: any) => {
+      console.log("Total is: " + result);
+    })
+    .catch( (error: any) => console.log("Error!!: ", error))
+  }
+
+  testSubstract(a: string, b: string) {
+    let data = {
+      param1: a,
+      param2: b
+    }
+
+    cordova.plugins.MathCalculator.substract(data).then( (result: any) => {
+      console.log("Total is: " + result);
+    })
+    .catch( (error: any) => console.log("Error!!: ", error))
+  }
 
 }
